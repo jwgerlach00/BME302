f2fb565c7ed622c75e26d5ae4bd7bad61fcdce68
jwgerlach00 <jwgerlach00@gmail.com>
2022-02-03 08:40:19 -0500

    Finished lab 2

--- lab2/windkessel_plots.py
+++ lab2/windkessel_plots.py
@@ -5,14 +5,14 @@ from scipy import integrate
 
 
 def moving_average(arr, window):
-    """Smooths out each column of an array using a moving average
+    """Smooths out each column of an array using a moving average.
 
     Args:
-        a (np.Array): Input array to be smoothed
-        window (int): Number of sequential points to average
+        a (np.Array): Input array to be smoothed.
+        window (int): Number of sequential points to average.
 
     Returns:
-        np.Array: Smoothed array
+        np.Array: Smoothed array.
     """
     length = round(len(arr)/window)
     out = []
@@ -24,6 +24,16 @@ def moving_average(arr, window):
 
 
 def discrete_bounded_integral(y_up, y_lo, x):
+    """Computes desired integral using pre-established datapoints and bounds.
+
+    Args:
+        y_up (iterable): Y values of the upper bound function.
+        y_lo (iterable): Same length as y_up. Y values of the lower bound function.
+        x (iterable): Same length as y_up and y_lo. X values.
+
+    Returns:
+        float: Integral value.
+    """
     upper = integrate.simps(y_up, x)
     lower = integrate.simps(y_lo, x)
     return upper - lower
@@ -42,7 +52,7 @@ if __name__ == '__main__':
     one_element_avg = moving_average(one_element_df.to_numpy(), 50)
     two_element_avg = moving_average(two_element_df.to_numpy(), 100)
 
-    # Plot Windkessel data
+    # Plot Windkessel data without integrals
     # plt.plot(one_element_avg[:, 0], one_element_avg[:, 1], lw=3, color='b')
     # plt.title('One-Element Windkessel', fontsize=24)
     # plt.xlabel('Time (s)', fontsize=16)
@@ -63,7 +73,7 @@ if __name__ == '__main__':
     # plt.savefig('2_element_plot.png')
     
     # Calculate integrals - experiment
-    min_one_el = min(one_element_avg[:, 1])*np.ones(len(one_element_avg))
+    min_one_el = one_element_avg[0, 1]*np.ones(len(one_element_avg))
     min_two_el = min(two_element_avg[:, 1])*np.ones(len(two_element_avg))
     one_element_integral = discrete_bounded_integral(one_element_avg[:, 1], min_one_el, one_element_avg[:, 0])
     two_element_integral = discrete_bounded_integral(two_element_avg[:, 1], min_two_el, two_element_avg[:, 0])
@@ -86,54 +96,48 @@ if __name__ == '__main__':
     plt.figure()  # 1-element
     plt.plot(one_element_avg[:, 0], one_element_avg[:, 1], lw=3, color='b', label='Smoothed data')
     plt.fill_between(one_element_avg[:, 0], min_one_el, one_element_avg[:, 1], color='orange', label='Integral')
-    plt.title('One-Element Experiment', fontsize=24)
+    plt.title('One-Element Windkessel Experiment', fontsize=24)
     plt.xlabel('Time (s)', fontsize=16)
     plt.ylabel('Voltage (mV)', fontsize=16)
     plt.xticks(fontsize=12)
     plt.yticks(fontsize=12)
     plt.grid(True)
-    plt.legend()
+    plt.legend(fontsize=12)
+    plt.savefig('1_element_exp.png')
     
     plt.figure()  # 2-element
     plt.plot(two_element_avg[:, 0], two_element_avg[:, 1], lw=3, color='r', label='Smoothed data')
     plt.fill_between(two_element_avg[:, 0], min_two_el, two_element_avg[:, 1], label='Integral')
-    plt.title('Two-Element Experiment', fontsize=24)
+    plt.title('Two-Element Windkessel Experiment', fontsize=24)
     plt.xlabel('Time (s)', fontsize=16)
     plt.ylabel('Voltage (mV)', fontsize=16)
     plt.xticks(fontsize=12)
     plt.yticks(fontsize=12)
     plt.grid(True)
-    plt.legend()
+    plt.legend(fontsize=12)
+    plt.savefig('2_element_exp.png')
     
     # Plot integral fills - simulation
     plt.figure()  # 1-element
     plt.plot(sim_df.t1, sim_df.y1, lw=3, color='b', label='Simulation data')
     plt.fill_between(sim_df.t1, sim_min, sim_df.y1, color='orange', label='Integral')
-    plt.title('One-Element Simulation Integral', fontsize=24)
+    plt.title('One-Element Windkessel Simulation', fontsize=24)
     plt.xlabel('Time (s)', fontsize=16)
-    plt.ylabel('Voltage (mV)', fontsize=16)
+    plt.ylabel('Pressure (mmHg)', fontsize=16)
     plt.xticks(fontsize=12)
     plt.yticks(fontsize=12)
     plt.grid(True)
-    plt.legend()
+    plt.legend(fontsize=12)
+    plt.savefig('1_element_sim.png')
     
     plt.figure()  # 2-element
     plt.plot(sim_df.t2, sim_df.y2, lw=3, color='r', label='Simulation data')
     plt.fill_between(sim_df.t2, sim_min, sim_df.y2, label='Integral')
-    plt.title('Two-Element Simulation Integral', fontsize=24)
+    plt.title('Two-Element Windkessel Simulation', fontsize=24)
     plt.xlabel('Time (s)', fontsize=16)
-    plt.ylabel('Voltage (mV)', fontsize=16)
+    plt.ylabel('Pressure (mmHg)', fontsize=16)
     plt.xticks(fontsize=12)
     plt.yticks(fontsize=12)
     plt.grid(True)
-    plt.legend()
-    
-    
-    # plt.figure()
-    # plt.plot(sim_df.t1, sim_df.y1, lw=3, color='blue')
-    # plt.plot(sim_df.t2, sim_df.y2, lw=3, color='red')
-    # plt.show()
-    
-    plt.show()
-    # print(one_sim_integral, two_sim_integral)
-    # print(sim_df.columns)
+    plt.legend(fontsize=12)
+    plt.savefig('2_element_sim.png')
