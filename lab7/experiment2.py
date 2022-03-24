979eb467fe04c8f88138e4988e047574f178eeeb
jacob <jwgerlach00@gmail.com>
2022-03-24 12:28:16 -0400

    html plot outputs

--- lab7/experiment2.py
+++ lab7/experiment2.py
@@ -8,4 +8,4 @@ for file in os.listdir(dir_path):
     print(file)
     df = pd.read_excel(f'{dir_path}/{file}')
     fig = px.line(df, x='time', y=['biceps', 'triceps', 'rms_biceps', 'rms_triceps'])
-    fig.show()  # Trial 0 is the good one
+    fig.write_html('data/ex2_html_plots/{0}.html'.format(file.split('.')[0]))  # Trial 2 is the good one
--- lab7/experiment4.py
+++ lab7/experiment4.py
@@ -1,3 +1,4 @@
+from doctest import testmod
 import os
 import pandas as pd
 import plotly.express as px
@@ -8,4 +9,4 @@ for file in os.listdir(dir_path):
     print(file)
     df = pd.read_excel(f'{dir_path}/{file}')
     fig = px.line(df, x='time', y='emg')
-    fig.show()  # Trial 0 is the good one
+    fig.write_html('data/ex4_html_plots/{0}.html'.format(file.split('.')[0]))
